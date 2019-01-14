7cd95fb768f1d41af17b5654a96b929fb7fac56a
Kyriakos Paschalidis <kyriakos.paschalidis@gmail.com>
2019-01-14 13:52:28 +0200

    Add support for getting recent events

---
+++ src/Resources/Event.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Themsaid\Forge\Resources;
+
+class Event extends Resource
+{
+
+    /**
+     * The id of the server where the event occured.
+     *
+     * @var integer
+     */
+    public $serverId;
+
+    /**
+     * The user that ran the event.
+     *
+     * @var string
+     */
+    public $ranAs;
+
+    /**
+     * The name of the server the event occured.
+     *
+     * @var string
+     */
+    public $serverName;
+
+    /**
+     * The description of the event.
+     *
+     * @var string
+     */
+    public $description;
+
+    /**
+     * The date/time the job was created.
+     *
+     * @var string
+     */
+    public $createdAt;
+
+}
