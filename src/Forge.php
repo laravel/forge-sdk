b897e959ed63cf4f115fffb39105b4f7502c0304
Ivan Torgov <likecod@gmail.com>
2020-03-21 13:05:57 +0300

    Implemented the webhook manager.

--- src/Forge.php
+++ src/Forge.php
@@ -14,6 +14,7 @@ class Forge
         Actions\ManagesWorkers,
         Actions\ManagesSSHKeys,
         Actions\ManagesRecipes,
+        Actions\ManagesWebhooks,
         Actions\ManagesMysqlUsers,
         Actions\ManagesCredentials,
         Actions\ManagesCertificates,
