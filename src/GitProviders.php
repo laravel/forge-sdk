38fb95b054431f7a003525f84a456d8c8a4d1d8a
victoravelar <deltatuts@gmail.com>
2020-08-15 17:09:18 +0200

    add forge supported git providers

---
+++ src/GitProviders.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Laravel\Forge;
+
+abstract class GitProviders
+{
+    const BITBUCKET = 'bitbucket';
+    const CUSTOM = 'custom';
+    const GITHUB = 'github';
+    const GITLAB = 'gitlab';
+}
