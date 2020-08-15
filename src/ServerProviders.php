1bff2e72f61baf157ac6f9b9f32c7e8f07f11200
victoravelar <deltatuts@gmail.com>
2020-08-15 17:08:22 +0200

    add forge known server (vps) providers

---
+++ src/ServerProviders.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Laravel\Forge;
+
+abstract class ServerProviders
+{
+    const AWS = 'aws';
+    const CUSTOM = 'custom';
+    const DIGITAL_OCEAN = 'ocean2';
+    const HETZNER = 'hetzner';
+    const LINODE = 'linode';
+    const VULTR = 'vultr';
+}
