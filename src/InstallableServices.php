84c35a67fa5246c4f7a409ac2219539262811d85
victoravelar <deltatuts@gmail.com>
2020-08-15 17:08:39 +0200

    add forge installable services

---
+++ src/InstallableServices.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Laravel\Forge;
+
+abstract class InstallableServices
+{
+    const PHP_56 = 'php56';
+    const PHP_70 = 'php70';
+    const PHP_71 = 'php71';
+    const PHP_72 = 'php72';
+    const PHP_73 = 'php73';
+    const PHP_74 = 'php74';
+
+    const MYSQL = 'mysql';
+    const MYSQL_8 = 'mysql8';
+
+    const MARIADB = 'mariadb';
+
+    const POSTGRES = 'postgres';
+}
