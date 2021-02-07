c20a3ad08d43a8073de3ebb785fd5f7de9ff237c
tfevens <tfevens@gmail.com>
2021-02-06 21:30:27 -0400

    Adds new serverTypes to Server create method.

---
+++ src/Resources/ServerTypes.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Laravel\Forge\Resources;
+
+class ServerTypes
+{
+    const APP = 'app';
+    const WEB = 'web';
+    const LOADBALANCER = 'loadbalancer';
+    const CACHE = 'cache';
+    const DATABASE = 'database';
+}
