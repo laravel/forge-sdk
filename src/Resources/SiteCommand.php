6c2ca293a849ed244a55230a09eba1391eb474c3
tfevens <tfevens@gmail.com>
2021-04-19 13:35:13 -0300

    Removes unused methods

--- src/Resources/SiteCommand.php
+++ src/Resources/SiteCommand.php
@@ -88,35 +88,4 @@ class SiteCommand extends Resource
      */
     public $output;
 
-    /**
-     * Create a new site command.
-     *
-     * @param  array  $data
-     * @return void
-     */
-    public function create(array $data)
-    {
-        $this->forge->createSiteCommand($this->serverId, $this->siteId, $data);
-    }
-
-    /**
-     * List all commands for a given server.
-     *
-     * @param  bool  $wait
-     * @return void
-     */
-    public function list($wait = true)
-    {
-        $this->forge->listCommandHistory($this->serverId, $this->siteId);
-    }
-
-    /**
-     * Get the output of the specified command.
-     *
-     * @return string
-     */
-    public function getCommand()
-    {
-        return $this->forge->getSiteCommand($this->serverId, $this->siteId, $this->id);
-    }
 }
