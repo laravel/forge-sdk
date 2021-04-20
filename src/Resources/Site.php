9fb380b8a24998327d8e0205975d235f0aad0061
tfevens <tfevens@gmail.com>
2021-04-20 15:28:42 -0300

    Adds missing properties from Site

--- src/Resources/Site.php
+++ src/Resources/Site.php
@@ -26,6 +26,13 @@ class Site extends Resource
     public $name;
 
     /**
+     * The aliases of the site.
+     *
+     * @var array
+     */
+    public $aliases;
+
+    /**
      * The name of the directory housing the site.
      *
      * @var string
@@ -114,21 +121,42 @@ class Site extends Resource
      *
      * @var string
      */
-    public $hipchatRoom;
-
-    /**
-     * Slack channel for deployment notifications.
-     *
-     * @var string
-     */
     public $slackChannel;
 
     /**
-     * The status of load balancing.
+     * Telegram chat ID for deployment notifications.
      *
      * @var string
      */
-    public $balancingStatus;
+    public $telegramChatId;
+
+    /**
+     * Telegram chat title (username) for deployment notifications.
+     *
+     * @var string
+     */
+    public $telegramChatTitle;
+
+    /**
+     * Microsoft Teams webhook URL for deployment notifications.
+     *
+     * @var string
+     */
+    public $teamsWebhookUrl;
+
+    /**
+     * Discord webhook URL for deployment notifications.
+     *
+     * @var string
+     */
+    public $discordWebhookUrl;
+
+    /**
+     * The username the site is running under.
+     *
+     * @var string
+     */
+    public $username;
 
     /**
      * The The date/time the site was created.
@@ -136,6 +164,27 @@ class Site extends Resource
      * @var string
      */
     public $createdAt;
+
+    /**
+     * The URL used to trigger a site deploy.
+     *
+     * @var string
+     */
+    public $deploymentUrl;
+
+    /**
+     * Is the site using HTTPS.
+     *
+     * @var bool
+     */
+    public $isSecured;
+
+    /**
+     * The aliases of the site.
+     *
+     * @var array
+     */
+    public $tags;
 
     /**
      * Refresh the site token.
