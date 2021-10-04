12149aea0210c90f8e0b556a93da806b3f74779d
Richard van Baarsen <baarsen@pm.me>
2021-10-04 10:28:50 +0200

    Add PHP 8 as an installable service

--- src/Resources/InstallableServices.php
+++ src/Resources/InstallableServices.php
@@ -10,6 +10,7 @@ class InstallableServices
     const PHP_72 = 'php72';
     const PHP_73 = 'php73';
     const PHP_74 = 'php74';
+    const PHP_80 = 'php80';
 
     const MYSQL = 'mysql';
     const MYSQL_8 = 'mysql8';
