79ab1e3e5026907cf03463ed1c659b5c26688e08
Nuno Maduro <enunomaduro@gmail.com>
2022-01-14 12:04:16 +0000

    Fixes updating site aliases

--- src/MakesHttpRequests.php
+++ src/MakesHttpRequests.php
@@ -68,9 +68,11 @@ trait MakesHttpRequests
      */
     protected function request($verb, $uri, array $payload = [])
     {
-        $response = $this->guzzle->request($verb, $uri,
-            empty($payload) ? [] : ['form_params' => $payload]
-        );
+        $payload = isset($payload['json'])
+            ? ['json' => $payload['json']]
+            : ['form_params' => $payload];
+
+        $response = $this->guzzle->request($verb, $uri, $payload);
 
         $statusCode = $response->getStatusCode();
 
