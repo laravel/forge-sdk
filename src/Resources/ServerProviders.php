e81f6cf7dc7c5c08ee4725df200037218c0fe8cb
James Brooks <james@alt-three.com>
2022-02-22 15:14:24 +0000

    Add updated server provider keys

--- src/Resources/ServerProviders.php
+++ src/Resources/ServerProviders.php
@@ -9,5 +9,7 @@ class ServerProviders
     const DIGITAL_OCEAN = 'ocean2';
     const HETZNER = 'hetzner';
     const LINODE = 'linode';
+    const LINODE4 = 'linode5';
     const VULTR = 'vultr';
+    const VULTR2 = 'vultr2';
 }
