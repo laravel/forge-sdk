e057610d1ddfd2f84ef7f16d0ce85b9bbfdb82c8
Pete Bishop <9081809+PeteBishwhip@users.noreply.github.com>
2026-05-18 13:43:02 +0100

    fix: align TeamInvitation with TeamInvitationResource spec
    Drop roleId and expiresAt (not in spec). Add organizationId so the
    SDK-injected context hydrates.

--- src/Resources/TeamInvitation.php
+++ src/Resources/TeamInvitation.php
@@ -7,9 +7,9 @@ namespace Laravel\Forge\Resources;
 class TeamInvitation extends Resource
 {
     /**
-     * The id of the team invitation.
+     * The id of the organization.
      */
-    public ?int $id = null;
+    public ?string $organizationId = null;
 
     /**
      * The id of the team.
@@ -17,24 +17,19 @@ class TeamInvitation extends Resource
     public ?int $teamId = null;
 
     /**
+     * The id of the team invitation.
+     */
+    public ?int $id = null;
+
+    /**
      * The email of the invitee.
      */
     public ?string $email = null;
 
     /**
-     * The id of the role.
-     */
-    public ?int $roleId = null;
-
-    /**
      * The date/time the team invitation was created.
      */
     public ?string $createdAt = null;
-
-    /**
-     * The date/time the team invitation expires.
-     */
-    public ?string $expiresAt = null;
 
     /**
      * The date/time the team invitation was last updated.
